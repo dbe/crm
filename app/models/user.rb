8d0dee8a74449a9c21a80c4cee9cf3d778f13744
Brian Ethier <davidbrianethier@gmail.com>
2017-02-24 23:24:56 -0700

    Making it more secure by requiring it to lookup by uuid

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,12 @@ class User < ApplicationRecord
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-      user = User.create(
-        name: data["name"],
-        email: data["email"],
-        password: Devise.friendly_token[0,20]
-      )
-    end
-
-    return user
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+    end 
   end
 end
