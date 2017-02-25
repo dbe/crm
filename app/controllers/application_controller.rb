9061598feff96bf38453643889a2f8e3556719d9
Brian Ethier <davidbrianethier@gmail.com>
2017-02-24 23:00:16 -0700

    adding method to user for omniauth, taking out oreo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :oreo
-
-  def oreo
-    puts "OREO"
-    puts request.env['omniauth.auth']
-  end
 end
